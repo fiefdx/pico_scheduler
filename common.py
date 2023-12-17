c0fb44be8b81154dcbc96840fc17cf2fcad53f4c
fiefdx <fiefdx@163.com>
2023-12-17 13:55:53 +0800

    remove hashlib

--- common.py
+++ common.py
@@ -7,13 +7,7 @@ try:
 except:
     platform = "micropython"
     print("micropython, no supervisor module exists, use time.ticks_ms instead")
-hashlib = None
-try:
-    import adafruit_hashlib as hashlib
-except:
-    import hashlib
 
-BUF_SIZE = 65536
 _TICKS_PERIOD = const(1<<29)
 _TICKS_MAX = const(_TICKS_PERIOD-1)
 _TICKS_HALFPERIOD = const(_TICKS_PERIOD//2)
@@ -45,54 +39,3 @@ def ticks_diff(ticks1, ticks2):
 def ticks_less(ticks1, ticks2):
     # "Return true iff ticks1 is less than ticks2, assuming that they are within 2**28 ticks"
     return ticks_diff(ticks1, ticks2) < 0
-
-
-def sha1sum(content):
-    #'''param content must be unicode, result is string'''
-    if platform == "circuitpython":
-        m = hashlib.sha1(content.encode("utf-8"))
-        m.digest()
-        result = m.hexdigest()
-    else:
-        m = hashlib.sha1(content.encode("utf-8"))
-        result = m.digest().hex()
-    return result
-
-
-def md5twice(content):
-    #'''param content must be unicode, result is string'''
-    if platform == "circuitpython":
-        m = hashlib.md5(content.encode("utf-8")).hexdigest()
-        result = hashlib.md5(m).hexdigest()
-    else:
-        result = None
-    return result
-
-
-def exists(path):
-    r = False
-    try:
-        if os.stat(path):
-            r = True
-    except OSError:
-        pass
-    return r
-
-
-def path_join(*args):
-    path = args[0]
-    for p in args[1:]:
-        if path.endswith("/"):
-            path = path[:-1]
-        p = p.strip("/")
-        if p.startswith(".."):
-            path = "/".join(path.split("/")[:-1])
-            path += "/" + p[2:]
-        else:
-            path += "/" + p
-    if args[-1].endswith("/"):
-        if not path.endswith("/"):
-            path += "/"
-    if not path.startswith("/"):
-        path = "/" + path
-    return path
